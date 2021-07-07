8e42e8846cf634772346543d14d7f00c85f46bd2
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-07 15:49:04 +0200

    interface för varuautomaten

---
+++ vending_machine/IVending.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Vending_machine.IVending {
+    interface IVending {
+	public Purchase();
+	public ShowAll();
+	public InsertMoney();
+	public EndTransaction();
+    }
+}
