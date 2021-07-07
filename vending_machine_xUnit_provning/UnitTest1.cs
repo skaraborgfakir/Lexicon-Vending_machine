931ae2c4f6b8cc6bbf4f67fd9df575f9d634f4e5
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-07 14:41:10 +0200

    bildning av solution för varuautomatprojektet
    inklusive projekt för xUnit:provning och specifikation för systemet

---
+++ vending_machine_xUnit_provning/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace vending_machine_xUnit_provning
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
