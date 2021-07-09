5bf3cdfd2b877aad120f0ecf7cfad62ef87e3e16
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-09 14:00:47 +0200

    Betalningsmedel
    uppräkning av vilka mynt och sedlar som accepteras

---
+++ vending_machine/Model/Betalningsmedel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Varuautomat.Betalningsmedel {
+    /// <summary
+    /// insättning från kund i saldo
+    /// </summary
+    class Inbetalt {
+
+    }
+
+    /// <summary
+    /// återbetalning till kund av kvarvarande saldo
+    /// betalar tillbaka i mynt
+    /// </summary
+    class Återbetalning {
+    }
+}
