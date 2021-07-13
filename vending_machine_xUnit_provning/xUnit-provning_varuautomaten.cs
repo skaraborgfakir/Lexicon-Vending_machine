4818af5909ed447701c47c05914e64ba1fd09104
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-07-13 18:31:51 +0200

    flyttat på test

--- vending_machine_xUnit_provning/xUnit-provning_varuautomaten.cs
+++ vending_machine_xUnit_provning/xUnit-provning_varuautomaten.cs
@@ -1,9 +1,11 @@
 using System;
 using Xunit;
+using System.Collections;
+using System.Collections.Generic;
 using Varuautomat.Modell;
 
 namespace Varuautomat.xUnit_provning {
-    public class Varuautomatprovning
+    public class VaruautomatVarulagerprovning
     {
 	[Theory]
 	[InlineData( "Te" )]
@@ -29,6 +31,30 @@ namespace Varuautomat.xUnit_provning {
 	    string[] allaProdukter = varuautomat.ShowAll();
 	    // Assert
 	    Assert.DoesNotContain( produktnamn, allaProdukter);
+	}
+    }
+
+    /// <summary> Testa att Varuautomaten rätt summerar pengarna
+    /// </summary>
+    public class VaruautomatKundsaldo
+    {
+	[Theory]
+	[ClassData(typeof(BlandadeValörerData))]
+	public void BlandadeValörer(int förväntat_belopp, string[] blandadeValörer) {
+	    // Arrange
+	    Modell.Varuautomat varuautomat = new Modell.Varuautomat();
+	    // Act
+	    foreach ( string valör in blandadeValörer)
+		varuautomat.InsertMoney( valör);
+	    //Assert
+	    Assert.Equal( förväntat_belopp, varuautomat.KundSaldo);
+	}
+	public class BlandadeValörerData : IEnumerable<object[]> {
+	    public IEnumerator<object[]> GetEnumerator() {
+		yield return new object[] { 280, new string[] { "Ingmar", "Astrid", "Astrid", "Astrid", "Astrid"}};
+		yield return new object[] { 480, new string[] { "Ingmar", "Ingmar", "Astrid", "Astrid", "Astrid", "Astrid"}};
+	    }
+	    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 	}
     }
 }
